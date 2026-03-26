469d44b7bbd1213a4c2800e01d3c3129cf6d1a33
Nithin Badigeri <badigeri.nithin@applexus.com>
2026-03-26 20:00:16 +0000

    Added Frontend Validations

--- main_project/webapp/controller/Login.controller.js
+++ main_project/webapp/controller/Login.controller.js
@@ -1,10 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("com.applexus.mainproject.controller.Login", {
         onInit() {
+        },
+        onLogin: function()
+        {
+            debugger; 
+            var userId = this.getView().byId("i1").getValue();
+            var password = this.getView().byId("i2").getValue(); 
+
+            userId = userId.trim();
+            password = password.trim();
+ 
+            if (!userId || !password) {
+                MessageToast.show("Please Enter both UserID and Password");
+                return; 
+            }
+            
+            if (!userId.endsWith("gmail.com")){
+                MessageToast.show("Please Enter a Valid Mail Id")
+            }           
+
         }
     });
 });
--- main_project/webapp/controller/Registration.controller.js
+++ main_project/webapp/controller/Registration.controller.js
@@ -1,10 +1,46 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller", 
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("com.applexus.mainproject.controller.Registration", {
         onInit() {
+        }, 
+        onRegister: function()
+        {
+            debugger; 
+            var fname = this.getView().byId("i3").getValue();
+            var mailId = this.getView().byId("i4").getValue(); 
+            var password = this.getView().byId("i5").getValue();
+            var cPassword = this.getView().byId("i6").getValue(); 
+            var phone = this.getView().byId("i7").getValue(); 
+
+            if (!fname || !mailId || !password || !cPassword){
+                MessageToast.show("Please Enter the Values in all Mandatory Fields"); 
+                return; 
+            } 
+
+            if (fname && mailId && password && cPassword){
+                   
+                if ( !mailId.endsWith("@gmail.com") )
+                {
+                   MessageToast.show("Please Enter a Valid Mail Id")
+                }
+                if ( password !== cPassword) 
+                {
+                    MessageToast.show("Both passwords must Match")
+                }
+
+            }
+
+
+
+
+
+
+
+
         }
     });
 });
