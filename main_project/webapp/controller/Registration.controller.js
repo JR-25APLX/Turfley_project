676039927ca427a2ae0bb37346d2e6633f5700b6
Nithin Badigeri <badigeri.nithin@applexus.com>
2026-03-26 16:27:17 +0000

    done with registration

---
+++ main_project/webapp/controller/Registration.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.applexus.mainproject.controller.Registration", {
+        onInit() {
+        }
+    });
+});
