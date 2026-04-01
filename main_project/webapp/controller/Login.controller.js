b34207704785a7aaf1a3b14ae00a84f77332021f
JOEL REJI <joel.reji@applexus.com>
2026-04-01 11:36:43 +0000

    Changed Login

--- main_project/webapp/controller/Login.controller.js
+++ main_project/webapp/controller/Login.controller.js
@@ -5,58 +5,59 @@ sap.ui.define([
     "sap/m/MessageBox"
 ], (Controller, JSONModel, MessageToast, MessageBox) => {
     "use strict";
-
     return Controller.extend("com.applexus.mainproject.controller.Login", {
         onInit() {
             var oJson = new JSONModel();
             oJson.setData({
                 user:{
-                   Userid: "",
+                   UserId: "",
                    Password: ""
                 }
             });
             this.getView().setModel(oJson);
         },
-
         onSignup: function(){
             var oRoute = this.getOwnerComponent().getRouter();
             oRoute.navTo("RouteReg");
         },
-
-
         onLogin: function()
         {
             var oJson = this.getView().getModel();
             var oDataModel = this.getOwnerComponent().getModel();
             var that = this;
          
-            var userId = this.getView().byId("i1").getValue();
-            var password = this.getView().byId("i2").getValue(); 
-
-            userId = userId.trim().toLowerCase();
-            password = password.trim();
+            var UserId = this.getView().byId("i1").getValue();
+            var Password = this.getView().byId("i2").getValue();
  
-            if (!userId || !password) {
+            UserId = UserId.trim().toLowerCase();
+            Password = Password.trim();
+ 
+            if (!UserId || !Password) {
                 MessageToast.show("Enter both UserID and Password");
-                return; 
+                return;
             }
-            
+           
             var emailRegex = /^[^\s@]+@[^\s@]+\.(com|in|org|net|co\.in)$/;
-                if (!emailRegex.test(userId)) {
+                if (!emailRegex.test(UserId)) {
                     MessageToast.show("Please Enter a Valid Email Id");
                     return;
-                }     
-            oJson.setProperty("/user/Userid", userId);
-            oJson.setProperty("/user/Password", password);
+                }    
+            oJson.setProperty("/user/UserId", UserId);
+            oJson.setProperty("/user/Password", Password);
             var oPayload = oJson.getProperty("/user");
             oDataModel.create("/LoginSet", oPayload, {
                 success: function(data) {
+ 
+ 
                     MessageToast.show("Status: " + data.Message);
                     if(data.Role === "A") {
                         that.getOwnerComponent().getRouter().navTo("RouteReg");
-                    } 
+                    }
                     else if(data.Role === "O"){
-                        that.getOwnerComponent().getRouter().navTo("RouteOwn1");
+               
+                        var oUserModel = new sap.ui.model.json.JSONModel({ OwnerId: data.UserId });                        
+                        sap.ui.getCore().setModel(oUserModel, "user");
+                        that.getOwnerComponent().getRouter().navTo("RouteOwnDash");
                     }
                     else if(data.Role === "U"){
                         that.getOwnerComponent().getRouter().navTo("RouteUser1");
@@ -66,7 +67,7 @@ sap.ui.define([
                     try {
                         var sEmsg = JSON.parse(oError.responseText).error.message.value;
                         MessageBox.error("Login Failed: " + sEmsg);
-                    } 
+                    }
                     catch (e) {
                         MessageBox.error("Connection Error");
                     }
