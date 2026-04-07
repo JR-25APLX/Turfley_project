4cc1c5f833cfc2a9a152d14e81df9c0420e4406b
JOEL REJI <joel.reji@applexus.com>
2026-04-07 05:36:49 +0000

    updated login and ownerdash

--- main_project/webapp/controller/OwnerDash.controller.js
+++ main_project/webapp/controller/OwnerDash.controller.js
@@ -6,7 +6,6 @@ sap.ui.define([
 
     return Controller.extend("com.applexus.mainproject.controller.OwnerDash", {
 
-        //Runs Automatically When this Page loads
         onInit: function () {
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.getRoute("RouteOwnDash").attachPatternMatched(
@@ -16,22 +15,17 @@ sap.ui.define([
 
         _onRouteMatched: function () {
 
-            // Getting the Logged In Details from the user Model
             var oUserModel = sap.ui.getCore().getModel("user");
 
-            // Here, OwnerId from login page
             var sOwnerId = oUserModel.getProperty("/OwnerId");
 
-            // Checking Whether Owner Id is containg any info or not
             if (!sOwnerId) {
                 console.error("OwnerId is empty!");
                 return;
             }
 
-            // Storing Locally for Future purpose
             this._sOwnerId = sOwnerId;
 
-            //Calling this Function
             this._loadTurfs(sOwnerId);
         },
 
@@ -41,11 +35,9 @@ sap.ui.define([
             var sPath = "/ZIB18_GRP1_OWNER(p_ownerid='" + sOwnerId + "')/Set";
 
 
-            // OData Call
             oDataModel.read(sPath, {
                 success: function (oData) {
 
-                    //Converting Backend Data into UI Data
                     var oTurfModel = new JSONModel({ turfs: oData.results });
                     this.getView().setModel(oTurfModel, "turfModel");
                 }.bind(this),
@@ -55,29 +47,23 @@ sap.ui.define([
             });
         },
 
-        // When Owner Wants to add a new Turf
         onAdd: function () {
             this.getOwnerComponent().getRouter().navTo("RouteOwnAddTurf");
         },
 
 
-        // When Owner Wants to edit the turf 
-        onEditTurf: function (oEvent) {
-            // It gives the info about the row clicked by Owner, 
+        onEditTurf: function (oEvent) { 
             var oContext = oEvent.getSource().getBindingContext("turfModel");
 
-            // Since oContext is pointing to the row
             var sTurfId = oContext.getProperty("Turf_Id");
             console.log("Navigating to edit turf:", sTurfId);
 
-            // Navigating to the Edit Screen with this TurfId
             this.getOwnerComponent().getRouter().navTo("RouteOwnEditTurf", {
                 turfId: sTurfId
             });
         },
 
 
-        // When Owner Wants to see his bookings
         onViewMyBookings: function () {
             this.getOwnerComponent().getRouter().navTo("RouteOwnTurfBooking");
         }
